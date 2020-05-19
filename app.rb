90f756b024f9ed1cb1454aea8a66f906fd3167ac
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-19 13:38:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ class App < Sinatra::Base
   post '/puppy' do
   @new_puppy Puppy.new(params(:))
     p = PigLatinizer.new
-    @piglatin = p.piglatinize(params[:name, :bree])
-    erb :piglatinize
+    @piglatin = p.piglatinize(params[:name,:breed,:age])
+    erb :display_puppy
   end
   
 end
